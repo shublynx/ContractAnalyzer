486c1116a2ee148be8b9d763d9e4949caed68f17
shublynx <shubham683075@gmail.com>
2026-03-25 01:14:11 +0530

    feat: implement pdf upload, text extraction, and swagger documentation

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,9 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from contracts.views import RegisterView, UserDetailView, ContractListCreateView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +16,12 @@ urlpatterns = [
     path('api/auth/register/', RegisterView.as_view(), name='auth_register'),
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # This downloads the raw schema file (YAML/JSON)
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    
+    # This is the actual Swagger UI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     
     # User Profile (Verification)
     path('api/auth/me/', UserDetailView.as_view(), name='auth_me'),
@@ -20,3 +29,6 @@ urlpatterns = [
     # Contracts Endpoints
     path('api/contracts/', ContractListCreateView.as_view(), name='contract-list-create'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
